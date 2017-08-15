be8358e0db87c244dbedfb9974ed7f04eb7c5c1f
Shipow <shipowlata@gmail.com>
2017-08-15 18:35:30 +0200

    Init

---
+++ src/App.vue
@@ -0,0 +1,134 @@
+<template>
+
+  <ais-index
+    app-id="T2ZX9HO66V"
+    api-key="7119d2f6f1cd95224251ec2e490e824f"
+    index-name="zelda_dev">
+
+    <div class="container">
+      <div class="columns is-multiline">
+        <section class="column is-12">
+          <ais-search-box></ais-search-box>
+        </section>
+
+        <aside class="column is-3">
+          <ais-tree-menu :attributes="['class','lvl1']" ></ais-tree-menu>
+
+          <h3>Tags</h3>
+          <ais-refinement-list attribute-name="tags" operator="and"></ais-refinement-list>
+        </aside>
+
+        <main class="column is-9">
+
+          <ais-results :results-per-page="100" class="hits-wrapper columns is-multiline">
+            <template scope="{ result }">
+              <div class="column is-3" :class="result.Type">
+                <div class="hit-wrapper">
+
+                  <div class="columns">
+                    <div class="column is-4 has-no-padding-bottom">
+                      <img :src="result.image" width=48 >
+                    </div>
+                    <div class="column is-8 has-text-right has-no-padding-bottom">
+                      <hp-meter :hp="result.hp" icon-width="20px"></hp-meter>
+                      <div v-if="result.strength" class="stat"><span class="stat-label">STR</span> <span class="stat-value">{{ result.strength }}</span></div>
+                      <div v-if="result.durability" class="stat"><span class="stat-label">DUR</span> <span class="stat-value">{{ result.durability }}</span></div>
+                      <div v-if="result.defense" class="stat"><span class="stat-label">DEF</span> <span class="stat-value">{{ result.defense }}</span></div>
+                      <div v-if="result.range" class="stat"><span class="stat-label">RNG</span> <span class="stat-value">{{ result.range }}</span></div>
+                      <div v-if="result.hp" class="stat"><span class="stat-label">Raw</span> <span class="stat-value">{{ result.hp }}</span><span class="stat-label"> HP</span></div>
+                    </div>
+                  </div>
+
+                  <h2><ais-highlight :result="result" attribute-name="name"></ais-highlight></h2>
+                  <p>{{result.description}}</p>
+                  <div class="spacer8"></div>
+                  <div class="spacer8"></div>
+                  <section v-if="result.hp_cooked || result.effect_cooked || result.duration_bonus_cooked" class="cooking">
+                    <div class="stat-label">Bonus when Cooked</div>
+                    <div class="spacer8"></div>
+                    <hp-meter v-if="result.hp_cooked" class="tag" :hp="result.hp_cooked" icon-width="16px"></hp-meter>
+                    <effect-meter v-if="result.effect_cooked" :effect="result.effect_cooked" :potential="result.potential" icon-width="16px"></effect-meter>
+                    <span v-if="result.duration_bonus_cooked" class="tag" v-for="time in result.duration_bonus_cooked "><img src="src/assets/icons/icon-time.svg" width="16px"> {{ time }}</span>
+                  </section>
+
+                  <div v-if="result.bonus"><span class="tag">{{ result.bonus }}</span></div>
+                  <div v-if="result.bonus_set"><span class="tag">Set: {{ result.bonus_set }}</span></div>
+
+                </div>
+              </div>
+            </template>
+          </ais-results>
+        </main>
+      </div>
+    </div>
+  </ais-index>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+
+<style lang="sass" src="bulma"></style>
+
+<style lang="sass">
+
+  $text-color: #E3E5C9
+
+  html
+    height: 100%
+
+  body
+    background: #050F2C
+    color: $text-color
+
+  //Bulma override
+  .hits-wrapper > .column
+    padding: 4px
+
+  .hit-wrapper
+    background: rgba(black,.4)
+    border-radius: 6px
+    padding: 16px
+    height: 100%
+    h2
+      font-size: 16px
+      font-weight: bold
+      margin-bottom: 12px
+      em
+        color: #B97433
+        font-style: normal
+    .tag
+      border-radius: 20px
+      border: solid 1px rgba($text-color,.1)
+      background: none
+      color: $text-color
+      padding: 0 8px
+    .stat-value
+      font-size: 14px
+      font-weight: bold
+    .stat-label
+      font-size: 10px
+      font-weight: bold
+      color: rgba($text-color,.7)
+      text-transform: uppercase
+    p
+      font-size: 12px
+      color: rgba($text-color,.5)
+
+  //helpers
+  .has-no-padding-top
+    padding-top: 0 !important
+  .has-no-padding-bottom
+    padding-bottom: 0 !important
+  .spacer8
+    height: 8px
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,97 @@
+import Vue from 'vue'
+import App from './App.vue'
+import InstantSearch from 'vue-instantsearch';
+
+Vue.use(InstantSearch);
+
+new Vue({
+  el: '#app',
+  render: h => h(App)
+})
+
+Vue.component('hp-meter', {
+  template: '<span v-html="hearth"></span>',
+  props: ['hp', 'icon-width'],
+  computed: {
+    hearth() {
+      var hearth = "";
+      var width = this.iconWidth;
+      var hp = parseFloat(this.hp) * 4;
+
+      if (hp) {
+        var hp_icons = [20,4,3,2,1];
+        while (hp !== 0){
+          hp_icons.forEach(
+            function(hp_icon){
+              if (hp / hp_icon >= 1){
+                var n = parseInt(hp / hp_icon);
+                hp = hp - (n * hp_icon);
+                for (var i = 0; i < n; i++){
+                  hearth += '<img src="src/assets/icons/icon-hp' + hp_icon + '.svg" width="' + width + '">';
+                }
+              }
+            }
+          )
+        }
+      }
+      return hearth;
+    }
+  }
+})
+
+Vue.component('effect-meter', {
+  template: '<span class="tag" v-html="x"></span>',
+  props: ['effect', 'potential', 'icon-width'],
+  computed: {
+    x() {
+      if (this.potential && this.effect) {
+
+        var width = this.iconWidth;
+        var potential = parseFloat(this.potential);
+        var effect = this.effect;
+        var x = '<img src="src/assets/icons/icon-effect-' + effect.toString().toLowerCase() + '.svg" width="' + width + '">&nbsp;' + effect;
+
+        var effects_config = {
+          energizing: {
+            short: "sta",
+            steps: [5,4,3,2,1]
+          },
+          hearthy: {
+            short: "extraHp",
+            steps: [20,4,3,2,1]
+          },
+          enduring: {
+            short: "sta",
+            steps: [5,4,3,2,1]
+          },
+          default: {
+            short: "lvl",
+            steps: [3,2,1]
+          }
+        };
+
+        if (effect === "energizing" || effect === "hearthy" || effect === "enduring") {
+            effect = effects_config[effect];
+        } else {
+            effect = effects_config.default;
+        }
+
+
+        while (potential !== 0){
+          effect.steps.forEach(
+            function(effect_icon){
+              if (potential / effect_icon >= 1){
+                var n = parseInt(potential / effect_icon);
+                potential = potential - (n * effect_icon);
+                for (var i = 0; i < n; i++){
+                  x += '&nbsp;<img src="src/assets/icons/icon-' + effect.short + effect_icon + '.svg" width="' + width + '">';
+                }
+              }
+            }
+          )
+        }
+        return x;
+      }
+    }
+  }
+})
