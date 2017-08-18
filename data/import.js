b205acd0cf522abbabea22f3f7f7b2730819c7a8
Shipow <shipowlata@gmail.com>
2017-08-19 01:08:47 +0200

    fix import script rate limit with cache + limits

--- data/import.js
+++ data/import.js
@@ -2,6 +2,7 @@
 let airtable = require('airtable');
 let algolia = require('algoliasearch');
 let async = require('async');
+// secret, edit with your own API credentials
 let config = require('../secret.config.json');
 
 let base = new airtable({apiKey: config.airtable.apiKey}).base(config.airtable.base);
@@ -10,44 +11,49 @@ let index = client.initIndex(config.algolia.index);
 
 let transformRecord = (record,relationalContent) => {
 	// Create an objectID from the primary key field of your row
-	record.fields.objectID = new Buffer(record.fields.name).toString('base64');
-	// Keep only the image url
-	record.fields.image = record.fields.image[0].url;
-	// Syntax for hierachical menu
-	if (typeof record.fields.subclass !== "undefined"){
-		record.fields.lvl1 = record.fields.class + ' > ' + record.fields.subclass;
-	}
-	// Flatten relations
-	if (relationalContent){
-		Object.keys(relationalContent).forEach((key,i) => {
-			record.fields[key]= relationalContent[key];
+	if (record && record.fields.name){
+		record.fields.objectID = new Buffer(record.fields.name).toString('base64');
+		// Keep only the image url
+		record.fields.image = record.fields.image[0].url;
+		// Syntax for hierachical menu
+		if (typeof record.fields.subclass !== "undefined"){
+			record.fields.lvl1 = record.fields.class + ' > ' + record.fields.subclass;
+		}
+		// Flatten relations
+		if (relationalContent){
+			Object.keys(relationalContent).forEach((key,i) => {
+				record.fields[key]= relationalContent[key];
+			});
+		}
+		// Add the object to algolia index (update if exists)
+		index.addObject(record.fields, (err, result) => {
+			index.waitTask(result.taskID, () => {
+				console.log('Indexed: '+ record.fields.name + ' – ' + record.fields.class  + ' – ' + record.fields.subclass);
+			});
 		});
 	}
-	// Add the object to algolia index (update if exists)
-	index.addObject(record.fields, (err, result) => {
-		index.waitTask(result.taskID, () => {
-			console.log('Indexed: '+ record.fields.name + ' – ' + record.fields.class  + ' – ' + record.fields.subclass);
-		});
-	});
 }
+
+// cache material relations to avoid too many calls to airtable
+var materialCache = {};
 
 let getRelations = (record, relations) => {
 	let materials = {};
-	async.each(relations, (relation, callback) => {
+	async.eachSeries(relations, (relation, callback) => {
 		let rel = relation.split(':');
 		if (typeof record.fields[rel[1]] !== "undefined"){
-			async.each(record.fields[rel[1]], (id, cb) => {
-				// it's a lot in parrallel, airtable is limited to 5 requests per seconds
-				getMaterial(id,rel[0], (material) => {
+			async.eachLimit(record.fields[rel[1]], 4,(id, cb) => {
+				// 4 in parrallel, airtable is limited to 5 requests per seconds
+				getMaterial(id,rel[0], materialCache, (material) => {
 					if (typeof materials[rel[1]] === 'undefined'){
 						materials[rel[1]] = [];
 					}
 					if (material.image){
-						let image = material.image[0].url;
+						materials[rel[1]].push({name:material.name, img: material.image[0].url});
 					} else {
-						let image = null
+						materials[rel[1]].push({name:material.name});
 					}
-					materials[rel[1]].push({name:material.name, img:image});
+					materialCache[id]= material;
 					cb();
 				});
 			}, (err) => {
@@ -59,21 +65,26 @@ let getRelations = (record, relations) => {
 		}
 	}, (err) => {
 		if (err) { console.error(err); return; }
-		transformRecord(record,materials);
+		transformRecord(record, materials);
 		return 'done'
 	});
 }
 
-let getMaterial = (id,relationTable, cb) => {
-	base(relationTable).find(id, (err, materialObject) => {
-		if (err) { console.error(err); return; }
-		cb(materialObject.fields);
-	});
+let getMaterial = (id,relationTable,materialCache, cb) => {
+	if (typeof materialCache[id] !== 'undefined' && typeof materialCache[id].name !== 'undefined'){
+		//console.log('Loaded from cache: ' + materialCache[id].name);
+		cb(materialCache[id]);
+	} else {
+		base(relationTable).find(id, (err, materialObject) => {
+			if (err) { console.error(err); return; }
+			cb(materialObject.fields);
+		});
+	}
 }
 
 let indexRecords = (table, relations) => {
 	base(table).select({
-		//maxRecords: 10, //debug
+		//maxRecords: 20, //debug
 		view: "Grid view"
 	}).eachPage(function page(records, fetchNextPage) {
 		records.forEach((record) => {
